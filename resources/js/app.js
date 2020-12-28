31e70b771f7625a86ffd8c6c8f58de457cd1f4fb
ykliba <mediaboxing2@gmail.com>
2020-12-28 12:59:09 +0900

    header&list

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,3 +1,6 @@
+import VueRouter from 'vue-router';
+import HeaderComponent from "./components/HeaderComponent";
+import TaskListComponent from "./components/TaskListComponent";
 /**
  * First we will load all of this project's JavaScript dependencies which
  * includes Vue and other libraries. It is a great starting point when
@@ -7,6 +10,19 @@
 require('./bootstrap');
 
 window.Vue = require('vue');
+
+Vue.use(VueRouter);
+
+const router = new VueRouter({
+        mode: 'history',
+        routes: [
+            {
+                path: '/tasks',
+                name: 'task.list',
+                component: TaskListComponent
+            },
+        ]
+});
 
 /**
  * The following block of code may be used to automatically register your
@@ -20,7 +36,7 @@ window.Vue = require('vue');
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
 
 Vue.component('example-component', require('./components/ExampleComponent.vue').default);
-
+Vue.component('header-component', HeaderComponent);
 /**
  * Next, we will create a fresh Vue application instance and attach it to
  * the page. Then, you may begin adding components to this application
@@ -29,4 +45,5 @@ Vue.component('example-component', require('./components/ExampleComponent.vue').
 
 const app = new Vue({
     el: '#app',
+    router
 });
